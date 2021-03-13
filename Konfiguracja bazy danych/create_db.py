15fe47a6ee79ef3a8f61a0546865fdb670f29107
plisek1986 <pawel.lisek.fp@gmail.com>
2021-03-13 14:08:29 +0100

    Adding comments to the complete code

--- Konfiguracja bazy danych/create_db.py
+++ Konfiguracja bazy danych/create_db.py
@@ -22,26 +22,31 @@ MESSAGES_TABLE = '''CREATE TABLE messages (
             FOREIGN KEY(to_id) REFERENCES users(id) ON DELETE CASCADE
              );'''
 
-
+#trying connection with the data base; before the db is created, Python should return
+#OperationalError (connection error)
 try:
     con = connect(user='postgres', password='coderslab', host='localhost', database='workshops_module2')
     con.autocommit = True
     cursor = con.cursor()
+    #trying to create a database
     try:
         cursor.execute(CREATE_DB)
         print('The database has been created')
     except DuplicateDatabase as e:
         print('Data base already exists!', e)
+    #trying to create a table for users and handling exception
     try:
         cursor.execute(USERS_TABLE)
         print('The table has been created')
     except DuplicateTable as e:
         print('Table already exists!', e)
+    # trying to create a table for messages and handling exception
     try:
         cursor.execute(MESSAGES_TABLE)
         print('The table has been created')
     except DuplicateTable as e:
         print('Table already exists!', e)
         con.close()
+# handling operational error
 except OperationalError as e:
     print('Connection error', e)
