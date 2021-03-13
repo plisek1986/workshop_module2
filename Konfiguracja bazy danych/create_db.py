ca3c7b411648c60e3f9bc2dda60669d0669744cf
plisek1986 <pawel.lisek.fp@gmail.com>
2021-03-13 14:02:42 +0100

    Creation of databases and first tables in the database v.2 - complete code

--- Konfiguracja bazy danych/create_db.py
+++ Konfiguracja bazy danych/create_db.py
@@ -1,4 +1,6 @@
-from psycopg2 import connect
+from psycopg2 import connect, OperationalError
+from psycopg2.errors import DuplicateDatabase, DuplicateTable
+
 
 
 CREATE_DB = 'CREATE DATABASE workshops_module2;'
@@ -7,7 +9,8 @@ USERS_TABLE = '''CREATE TABLE users (
             id serial not null,
             username varchar(255),
             hashed_password varchar(80),
-            PRIMARY KEY(id)'''
+            PRIMARY KEY(id)
+            );'''
 
 MESSAGES_TABLE = '''CREATE TABLE messages (
             id serial not null,
@@ -16,19 +19,29 @@ MESSAGES_TABLE = '''CREATE TABLE messages (
             creation_date timestamp DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY(id),
             FOREIGN KEY(from_id) REFERENCES users(id) ON DELETE  CASCADE,
-            FOREIGN KEY(to_id) REFERENCES users(id) ON DELETE CASCADE)
+            FOREIGN KEY(to_id) REFERENCES users(id) ON DELETE CASCADE
              );'''
 
-con = connect(user='postgres', password='coderslab', host='localhost', database='workshops_module2')
-con.autocommit = True
-cursor = con.cursor()
-cursor.execute(CREATE_DB)
 
-#
-# def new_database(db_name):
-#     cursor = con.cursor()
-#     try:
-#         cursor.execute('CREATE DATABASE %s', (db_name,))
-#         db = cursor.fetchone()
-#     raise DuplicateDatabase('The database already exists!')
-
+try:
+    con = connect(user='postgres', password='coderslab', host='localhost', database='workshops_module2')
+    con.autocommit = True
+    cursor = con.cursor()
+    try:
+        cursor.execute(CREATE_DB)
+        print('The database has been created')
+    except DuplicateDatabase as e:
+        print('Data base already exists!', e)
+    try:
+        cursor.execute(USERS_TABLE)
+        print('The table has been created')
+    except DuplicateTable as e:
+        print('Table already exists!', e)
+    try:
+        cursor.execute(MESSAGES_TABLE)
+        print('The table has been created')
+    except DuplicateTable as e:
+        print('Table already exists!', e)
+        con.close()
+except OperationalError as e:
+    print('Connection error', e)
