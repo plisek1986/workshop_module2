da31579a194d381bdd1e832ea3b2f48cce973271
plisek1986 <pawel.lisek.fp@gmail.com>
2021-03-14 09:59:51 +0100

    First lines of code for the Message class

--- Konfiguracja bazy danych/models.py
+++ Konfiguracja bazy danych/models.py
@@ -1,22 +1,29 @@
-import random
+from clcrypto import hash_password
 from psycopg2 import connect
 
 
 class User:
+    """
+    This class defines actions for user and password management
+    """
+    # define the connection with database
     con = connect(user='postgres', host='localhost', password='coderslab',
                   database='workshops_module2')
     con.autocommit = True
     cursor = con.cursor()
 
+    # define init method
     def __init__(self, username='', hashed_password=''):
         self._id = -1
         self.username = username
         self._hashed_password = hashed_password
 
+    # define dynamic properties for sensitive data
     @property
     def get_id(self):
         return self._id
 
+    # define dynamic properties for sensitive data
     @property
     def hashed_password(self):
         return self._hashed_password
@@ -26,44 +33,104 @@ class User:
     def set_password(self, password, salt=''):
         self._hashed_password = hash_password(password, salt)
 
+    # define setter for dynamic property of password
     @hashed_password.setter
     def hashed_password(self, password):
         self.set_password(password)
 
+    # define method for saving a new user to the database
     def save_to_db(self, cursor):
         if self._id == -1:
             cursor.execute('''INSERT INTO users (username, hashed_password) VALUES (%s, %s)
                            RETURNING id''', (self.username, self.hashed_password))
             self._id = cursor.fetchone()[0]
+        else:
+            cursor.execute(
+                '''UPDATE users SET username=self.username hashed_password=self.hashed_password WHERE id=self._id ''')
+            cursor.fetchone()
             return True
         return False
 
+    # define a static method for loading a user from the
+    # database using his/her username as an atribute
     @staticmethod
-    def load_user_by_username(cursor, name):
+    def load_user_by_username(cursor, username):
         cursor.execute('SELECT id, username, hashed_password FROM users where username=%s',
-                       (name,))
+                       (username,))
         data = cursor.fetchone()
         if data:
-            id, username, hashed_password = data
+            id_, username, hashed_password = data
             loaded_user = User(username)
-            loaded_user._id = id
+            loaded_user._id = id_
             loaded_user._password = hashed_password
             return loaded_user
         return None
 
+    # define a static method for loading a user from the
+    # database using his/her id as an atribute
     @staticmethod
-    def load_user_by_username(cursor, id):
+    def load_user_by_id(cursor, id_):
         cursor.execute('SELECT id, username, hashed_password FROM users where username=%s',
-                       (id,))
+                       (id_,))
         data = cursor.fetchone()
         if data:
-            id, username, hashed_password = data
+            id_, username, hashed_password = data
             loaded_user = User(username)
-            loaded_user._id = id
-            loaded_user._password = hashed_password
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
             return loaded_user
         return None
 
+    # define a static method for loading all users from the database
+    @staticmethod
+    def load_all_users(cursor):
+        users = []
+        cursor.execute('SELECT id, username, hashed_password FROM users')
+        for row in cursor.fetchall():
+            id_, username, hashed_password = row
+            loaded_user = User()
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            users.append(loaded_user)
+        return users
+
+    # define a method for deleting users from the database = THIS IS NOT A STATIC METHOD!!
+    def delete(self, cursor):
+        cursor.execute('DELETE * FROM users WHERE id=%s', (self._id,))
+        self._id = -1
+        return True
+
+
+class Messages:
+
+    con = connect(user='postgres', host='localhost', password='coderslab',
+                  database='workshops_module2')
+    con.autocommit = True
+    cursor = con.cursor()
+
+    def __init__(self, from_id, to_id, text, creation_data):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self.creation_data = creation_data
+
+    @property
+    def id(self):
+        return self._id
+
+    def save_to_db(self):
+        if self._id == -1:
+            cursor.execute('''INSERT INTO messages (from_id, to_id, text, creation_data) VALUES (%s, %s, %s, %s)
+                           RETURNING id''', (self.from_id, self.to_id, self.text, self.creation_data))
+            self._id = cursor.fetchone()[0]
+        else:
+            cursor.execute(
+                '''UPDATE messages SET from_id=self.from_id to_id=self.to_id text=self.text
+                creation_data=self.creation_data WHERE id=self._id ''')
+            cursor.fetchone()
+            return True
+        return False
 
 # def hash_password(password, salt=None):
 # """
